d6ce83d1e4bae854e898ee651b65a3fcd3edca91
TurkCoderv1 <turkbahr19@hotmail.com>
2021-05-29 00:10:40 +0300

    operations

--- src/app/services/animal.service.ts
+++ src/app/services/animal.service.ts
@@ -5,21 +5,29 @@ import { ListResponseModel } from '../models/listResponseModel';
 import { Animal } from '../models/animal';
 import { ResponseModel } from '../models/responseModel';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnimalService {
-  apiUrl="https://localhost:44391/api/"; //link ekle
-  constructor(private httpClient: HttpClient) { }
-  getAnimals():Observable<ListResponseModel<Animal>> {
-    let newPath = this.apiUrl+"animals/getall"
+  apiUrl = 'https://localhost:44335/api/'; //link ekle
+  constructor(private httpClient: HttpClient) {}
+  
+  getAnimals(): Observable<ListResponseModel<Animal>> {
+    let newPath = this.apiUrl + 'animals/getall';
     return this.httpClient.get<ListResponseModel<Animal>>(newPath);
-      }
-  getAnimalsByAnimalType(animalTypeId:number):Observable<ListResponseModel<Animal>> {
-    let newPath = this.apiUrl+"animals/getbyanimaltype?animalTypeId="+animalTypeId
-    return this.httpClient.get<ListResponseModel<Animal>>(newPath);
-      }
-      add(animal:Animal):Observable<ResponseModel>{
-        return this.httpClient.post<ResponseModel>(this.apiUrl+"animal/add",animal)
-      }           
+  }
 
+  getAnimalsByAnimalType(
+    animalTypeId: number
+  ): Observable<ListResponseModel<Animal>> {
+    let newPath =
+      this.apiUrl + 'animals/getbyanimaltype?animalTypeId=' + animalTypeId;
+    return this.httpClient.get<ListResponseModel<Animal>>(newPath);
+  }
+
+  add(animal: Animal): Observable<ResponseModel> {
+    return this.httpClient.post<ResponseModel>(
+      this.apiUrl + 'animal/add',
+      animal
+    );
+  }
 }
